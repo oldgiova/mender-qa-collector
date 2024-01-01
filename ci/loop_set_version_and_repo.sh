d360ecbc31c10bf9de2087b96c259a775420584f
Roberto Giovanardi <rob.giovanardi@gmail.com>
2024-01-01 22:33:48 +0100

    WIP substitution working docker compose

--- ci/loop_set_version_and_repo.sh
+++ ci/loop_set_version_and_repo.sh
@@ -4,4 +4,35 @@
 #  mender-qa/integration/extra/release_tool.py --set-version-of $repo --version $1 --repository registry.gitlab.com/northern.tech/mender
 #done
 
-find mender-qa/integration -type f -iname 'docker-compose.*yml' -exec sed -i -r "s/(^\s*image:\s*)(mendersoftware|registry\.mender\.io)(\/mendersoftware\/|\/)(\S+):(\S+)$/\1registry.gitlab.com\/northern.tech\/mender\/\4\:master/" {} \; 
+find mender-qa/integration \
+  -type f \
+  -iname 'docker-compose.*yml' \
+  -exec \
+    sed \
+      --in-place \
+      --regexp-extended "s/(^\s*image:\s*)(mendersoftware|registry\.mender\.io)(\/mendersoftware\/|\/)(\S+):(\S+)$/\1registry.gitlab.com\/northern.tech\/mender\/\4\:master/" {} \; 
+
+# restore previous mender-test-container image
+find mender-qa/integration \
+  -type f \
+  -iname 'docker-compose.*yml' \
+  -exec \
+    sed \
+      --in-place \
+      --regexp-extended "s/(^\s*image:\s*)(registry\.gitlab\.com\/northern\.tech\/mender\/mender-test-containers:\S+)$/\1mendersoftware\/mender-test-containers:backend-integration-testing/" {} \; 
+
+# fix workflows registry
+find mender-qa/integration \
+  -type f \
+  -iname 'docker-compose.*yml' \
+  -exec \
+    sed \
+      --in-place \
+      --regexp-extended "s/(^\s*image:\s*)(registry\.gitlab\.com)(\/northern\.tech\/mender\/)(workflows-worker):(\S+)$/\1\2\3workflows:\5/" {} \; 
+
+
+find mender-qa/integration \
+  -type f \
+  -iname 'docker-compose.*yml' \
+  -exec \
+    grep 'image:' {} \;
